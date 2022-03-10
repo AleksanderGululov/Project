951781f98ed0bf73367332b863a4cd5742b4e652
Toshiba <example@gmail.com>
2022-03-10 12:51:41 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+//alert('Hello'); 
+
+//const result = confirm('are you here?');
+//const answer = prompt('Вам есть 18?', '18');
+//console.log(answer);
+const answers = [];
+answers [0] = prompt('Как ваше имя?', '');
+answers [1] = prompt('Как ваша фамилия?', '');
+answers [2] = prompt('сколько вам лет?', '');
+
+document.write(answers);
